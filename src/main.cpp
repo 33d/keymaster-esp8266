ae1fecda3825348c0551350ea64dc41eeb2464d2
Damien <damien@sail>
2016-11-10 21:57:37 +1030

    RFID card IDs are read

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,32 @@
 
 File apFile;
 
+MFRC522 mfrc522(D2, D1);
+
 const char* host = "members.hackadl.org";
 const char* fingerprint = "15:15:AA:69:72:8C:09:7C:E5:17:27:38:B3:2A:39:89:BC:65:3C:A9";
 
 // The WL_ constants, for debug output (from wl_definitions.h)
 const char* wl_status[] = { "IDLE_STATUS", "NO_SSID_AVAIL", "SCAN_COMPLETED",
   "CONNECTED", "CONNECT_FAILED", "CONNECTION_LOST", "DISCONNECTED" };
+
+void ShowReaderDetails() {
+  // Get the MFRC522 software version
+  byte v = mfrc522.PCD_ReadRegister(mfrc522.VersionReg);
+  Serial.print(F("MFRC522 Software Version: 0x"));
+  Serial.print(v, HEX);
+  if (v == 0x91)
+    Serial.print(F(" = v1.0"));
+  else if (v == 0x92)
+    Serial.print(F(" = v2.0"));
+  else
+    Serial.print(F(" (unknown)"));
+  Serial.println("");
+  // When 0x00 or 0xFF is returned, communication probably failed
+  if ((v == 0x00) || (v == 0xFF)) {
+    Serial.println(F("WARNING: Communication failure, is the MFRC522 properly connected?"));
+  }
+}
 
 void setup() {
   Serial.begin(115200);
@@ -23,13 +43,20 @@ void setup() {
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
 
+  SPI.begin(); // for MFRC522
+  delay(50);
+  mfrc522.PCD_Init();
+  delay(50);
+  ShowReaderDetails();
+
   if (!SPIFFS.begin())
     log.println("SPIFFS begin failed!");
 
   apFile = SPIFFS.open("/ap", "r");
 }
 
-void check_card_reader() {
+void send_card() {
+  // TODO
   static bool runOnce = false;
   while (!runOnce) {
     WiFiClientSecure client;
@@ -60,6 +87,17 @@ void check_card_reader() {
   }
 
   runOnce = true;
+}
+
+void check_card_reader() {
+  if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial())
+    return;
+
+  log.print("Scanned PICC's UID: ");
+  for (int i = 0; i < 7; i++)
+    log.print(mfrc522.uid.uidByte[i], HEX);
+  log.println();
+  mfrc522.PICC_HaltA();
 }
 
 void connect_to_next_access_point() {
